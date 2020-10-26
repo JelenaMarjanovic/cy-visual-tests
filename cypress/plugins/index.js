a713560612f7e54347f754872dd4151fa05b658e
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2020-10-26 02:29:03 +0100

    Percy - Integration with Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,9 +2,13 @@ const {
   addMatchImageSnapshotPlugin,
 } = require('cypress-image-snapshot/plugin');
 
+const percyHealthCheck = require('@percy/cypress/task');
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config);
+
+  on('task', percyHealthCheck);
 };
